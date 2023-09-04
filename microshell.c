2a7ed9486866a56349869f9ccca57c2d0f77ff8a
Roberto Abril hidalgo <rabril-h@car9s6.42barcelona.com>
2023-09-04 22:02:48 +0200

    Added comments

--- microshell.c
+++ microshell.c
@@ -6,7 +6,7 @@
 /*   By: rabril-h <rabril-h@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 21:02:06 by rabril-h          #+#    #+#             */
-/*   Updated: 2023/08/28 22:28:03 by rabril-h         ###   ########.fr       */
+/*   Updated: 2023/09/04 22:02:13 by rabril-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,59 +18,99 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+/**
+ * @brief simple function to count char. We are not using
+ * the *str++ approach because we want to preserve the string
+ * integrity
+ * 
+ * @param str 
+ * @return int 
+ */
 
-int	ft_strlen(char *str)
+int	msh_strlen(char *str)
 {
-	int	i = 0;
+	int	i;
+
+	i = 0;
 	while (str[i])
 		i++;
 	return (i);
 }
 
-int	ft_putstr_fd2(char *str)
+/**
+ * @brief simple function to output a string on the
+ * standard output as error. Write as much chars as
+ * the length of the string passed as argument
+ * 
+ * @param str 
+ * @return int 
+ */
+
+int	msh_error(char *str)
 {
-	write(2, str, ft_strlen(str));
-		return (1);
+	write(2, str, msh_strlen(str));
+	return (1);
 }
 
-int	ft_execute(char *argv[], int i, int tmp_fd, char *env[])
+/**
+ * @brief function that takes care of the execution (execve)
+ * of commands
+ * 
+ * This funcion takes a string which can be the whole string or until
+ * it reaches a ; (not included in the string)
+ * 
+ * The function tries to to execute the command with the name passed as argv[0]
+ * which is the first portion of the passes string followed by the rest of the chain string
+ * 
+ * If the execution is successful the control is sent back to the program and no error is printed (everything after execve line)
+ * 
+ * 
+ * 
+ * @param argv 
+ * @param i 
+ * @param tmp_fd 
+ * @param env 
+ * @return int 
+ */
+
+int	msh_exec(char *argv[], int i, int tmp_fd, char *env[])
 {
-	argv[i] = NULL; 
-	dup2(tmp_fd, 0);
-	close(tmp_fd);
+	argv[i] = NULL; // ? This closes the string chain so we do not overflow 
+	dup2(tmp_fd, 0); // ? This makes our tmp_fd the same as the standard input like if we are writting text
+	close(tmp_fd); // ? We close our tmp_fd
 	execve(argv[0], argv, env);
-	ft_putstr_fd2("error: cannot execute ");
-	ft_putstr_fd2(argv[0]);
-	ft_putstr_fd2("\n");
+	msh_error("error: cannot execute ");
+	msh_error(argv[0]);
+	msh_error("\n");
 	return (1);
 }
 
 int	main(int argc, char *argv[], char *env[])
 {
-	int	i;
-	int	fd[2];
-	int	tmp_fd;
-	int	pid;
+	int	i; // ? we need a counter
+	int	fd[2]; // ? we need a couple of file descriptors for pipes
+	int	tmp_fd; // ? we need a temporary fd
+	int	pid; // ? we need our reference for the process
 
-	(void)argc; // is needed in exam, because the exam tester compiles with -Wall -Wextra -Werror
-	i = 0;
-	tmp_fd = dup(0);
+	(void)argc; // ? is needed in exam, because the exam tester compiles with -Wall -Wextra -Werror
+	i = 0; // ? Set counter
+	tmp_fd = dup(0); // ? set our tmp_fd as the standard input since we will be sending from other place. We store the position.
+	// ? Loop through as long as we a char and anything after that
 	while (argv[i] && argv[i + 1])
 	{
-		argv = &argv[i + 1];
-		i = 0;
+		argv = &argv[i + 1]; // ? We move the pointer as we go through the string chain. This can only happen if there is actually to loop through thanks to our previous while
+		i = 0; // ? Reset the counter once the pointer has been moved so no need for another counter such as j or whatever
 		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
-			i++;
-		if (strcmp(argv[0], "cd") == 0) //cd
+			i++; // ? increment the counter as long as there is a char and that char is NOT ; AND is NOT | (strcmp returns 1 when there is no match)
+		if (!strcmp(argv[0], "cd")) // ? if the first argument (command) for the stirng chain is cd
 		{
-			if (i != 2) // mas de 2 argumentos suponiendo que el primero es cd
-				ft_putstr_fd2("error: cd: bad arguments\n");
-			else if (chdir(argv[1]) == -1)
+			if (i != 2) // ? more or less than 2 arguments for cd is invalid, hence print error
+				msh_error("error: cd: bad arguments\n");
+			else if (chdir(argv[1]) == -1) // ? If we try to change the directory and there is an error, print error (the dir does not actually change in microshell. Do not freak out)
 			{
-
-				ft_putstr_fd2("error: cd: cannot change directory to ");
-					ft_putstr_fd2(argv[1]);
-					ft_putstr_fd2("\n");
+				msh_error("error: cd: cannot change directory to ");
+				msh_error(argv[1]);
+				msh_error("\n");
 			}
 		}
 		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0)) //exec in stdout
@@ -80,7 +120,7 @@ int	main(int argc, char *argv[], char *env[])
 				return 1;
 			if (pid == 0)
 			{
-				if (ft_execute(argv, i, tmp_fd, env))
+				if (msh_exec(argv, i, tmp_fd, env))
 					return (1);
 			}
 			else
@@ -98,7 +138,7 @@ int	main(int argc, char *argv[], char *env[])
 			{
 				dup2(fd[1], 1);
 				close(fd[0]);
-				if (ft_execute(argv, i, tmp_fd, env))
+				if (msh_exec(argv, i, tmp_fd, env))
 					return (1);
 			}
 			else
@@ -108,7 +148,7 @@ int	main(int argc, char *argv[], char *env[])
 				tmp_fd = fd[0];
 			}
 		}
-	}
-	close(tmp_fd);
-	return (0);
+	} // ? End of main loop
+	close(tmp_fd); // ? we close our tmp_fd
+	return (0); // ? default return as sucess
 }
